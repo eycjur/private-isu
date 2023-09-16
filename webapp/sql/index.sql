0904a097c77604a44731f4c424578d4b28fec2b3
eycjur <eycjur@gmail.com>
2023-09-16 21:41:24 +0900

    feat: postのuser_idにindexを追加

--- webapp/sql/index.sql
+++ webapp/sql/index.sql
@@ -2,4 +2,5 @@ USE isuconp;
 
 ALTER TABLE comments ADD INDEX idx_post_id_created_at(post_id, created_at);
 ALTER TABLE comments ADD INDEX idx_user_id(user_id);
-ALTER TABLE posts ADD INDEX idex_created_at(created_at);
+ALTER TABLE posts ADD INDEX idx_created_at(created_at);
+ALTER TABLE posts ADD INDEX idx_user_id(user_id);
