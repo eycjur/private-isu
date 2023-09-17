ca8f22355f79e980161d0c3ed1bf4240795264ff
katsuhiro.muto <katsuhiro.muto@brainpad.co.jp>
2023-09-17 12:45:58 +0900

    feat: userのカラムを必要なものに絞る

--- webapp/python/main.py
+++ webapp/python/main.py
@@ -144,12 +144,7 @@ def make_posts(results, all_comments=False):
             """
             SELECT comments.*, 
                 JSON_OBJECT(
-                    'id', users.id,
-                    'account_name', users.account_name,
-                    'passhash', users.passhash,
-                    'authority', users.authority,
-                    'del_flg', users.del_flg,
-                    'created_at', users.created_at
+                    'account_name', users.account_name
                 ) AS user
             FROM comments
             JOIN users on users.id = comments.user_id
